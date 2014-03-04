afece036e2dc7a0296e7aef9e79aec0e4c4dc7a0
Yanus Poluektovich <ypoluektovich@gmail.com>
2014-03-04 16:00:21 +0400

    use slf4j in server code

--- src/main/java/org/shoushitsu/wordpress/dump/server/Main.java
+++ src/main/java/org/shoushitsu/wordpress/dump/server/Main.java
@@ -3,11 +3,15 @@ package org.shoushitsu.wordpress.dump.server;
 import org.simpleframework.http.core.ContainerServer;
 import org.simpleframework.transport.connect.Connection;
 import org.simpleframework.transport.connect.SocketConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Main {
+
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
 		if (args.length != 1) {
@@ -16,35 +20,34 @@ public class Main {
 		}
 
 		RequestHandler handler = new RequestHandler();
+
 		Connection connection;
 		try {
 			ContainerServer server = new ContainerServer(handler);
 			connection = new SocketConnection(server);
 			connection.connect(new InetSocketAddress(Integer.parseInt(args[0])));
 		} catch (IOException e) {
-			System.err.println("Failed to start server");
-			e.printStackTrace();
+			log.error("Failed to start the server", e);
 			System.exit(1);
 			return;
 		}
 
-		System.err.println("Started server");
+		log.info("Started the server");
 
 		try {
 			handler.awaitShutdown();
 		} catch (InterruptedException e) {
-			System.err.println("Interrupted while waiting for shutdown");
-			e.printStackTrace();
+			log.error("Interrupted while waiting for shutdown", e);
 			return;
 		}
 
+		log.warn("Stopping the server");
 		try {
 			connection.close();
 		} catch (IOException e) {
-			System.err.println("Error while closing connection");
-			e.printStackTrace();
+			log.error("Error while shutting the server down", e);
 		}
-		System.err.println("Exiting");
+		log.info("Sayonara");
 	}
 
 }
--- src/main/java/org/shoushitsu/wordpress/dump/server/RequestHandler.java
+++ src/main/java/org/shoushitsu/wordpress/dump/server/RequestHandler.java
@@ -4,11 +4,15 @@ import org.simpleframework.http.Path;
 import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
 import org.simpleframework.http.core.Container;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 class RequestHandler implements Container {
+
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
 	private final CountDownLatch shutdownLatch = new CountDownLatch(1);
 
@@ -19,26 +23,25 @@ class RequestHandler implements Container {
 	@Override
 	public void handle(Request req, Response resp) {
 		Path path = req.getAddress().getPath();
-
-		System.err.println("Request: " + path);
-
+		log.info("Request: " + path);
 		try {
 			switch (path.getPath()) {
 				case "/stop":
+					log.info("Server stop command received");
 					resp.setCode(200);
 					shutdownLatch.countDown();
 					break;
 				default:
+					log.warn("Unsupported request: " + path);
 					break;
 			}
 		} finally {
 			try {
 				resp.close();
 			} catch (IOException e) {
-				e.printStackTrace();
+				log.error("Error while closing a response", e);
 			}
 		}
-
 	}
 
 }
